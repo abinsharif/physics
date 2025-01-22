9c2286236910518c20a3c1bd2c47dd89e28740dc
abinsharif <a.binsharif1@gmail.com>
2025-01-22 10:20:30 +0600

    Start

---
+++ server.py
@@ -0,0 +1,10 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/')
+def serve_page():
+    return send_from_directory('.', 'physics.html')
+
+if __name__ == '__main__':
+    app.run(host='192.168.0.232', port=1234, debug=True)
